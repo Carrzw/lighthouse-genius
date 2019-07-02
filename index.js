3cc450cd418d782be51500f24a060044f2929410
Wang Carr <carr.wang@tarokosoftware.com>
2019-07-02 17:09:15 +0800

    single thread version - integration with pkg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const lighthouse = require('lighthouse');
 const csvParse     = require('csv-parse/lib/sync');
 const csvStringify = require('csv-stringify/lib/sync');
 
-const fs = require('fs');
+const fs      = require('fs');
 const { URL } = require('url');
 
 // lighthouse result return both "title"(for display) and "name", we use name as key for mapping data later
@@ -52,7 +52,7 @@ const auditsNameTitleMap = {
 const inputColumnsHeader = ['Device', 'URL'];
 // use orderMap to record the index of each "name", lighthouse result 
 // from target urls could use this index to find correct column
-const orderMap         = {};
+const orderMap = {};
 Object.keys({ ...categoriesNameTitleMap, ...auditsNameTitleMap }).forEach((element, i) => {
   orderMap[element] = i + inputColumnsHeader.length;
 });
@@ -74,77 +74,82 @@ let config = {
 
 const startTime = Date.now();
 
-(async() => {  
+module.exports = function(inputFilePath) {
+  (async() => {
 
-  input = fs.readFileSync('./input.csv', { encoding: 'utf8' });
-  
-  const auditTargets = csvParse(input, {
-    columns:          true,
-    skip_empty_lines: true,
-    trim:             true,
-  });
-  const totalTasks   = auditTargets.length;
-  let countingDown   = totalTasks;
+    input = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
+    
+    const auditTargets = csvParse(input, {
+      columns:          true,
+      skip_empty_lines: true,
+      trim:             true,
+    });
+    const totalTasks   = auditTargets.length;
+    let countingDown   = totalTasks;
 
-  // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
-  const browser = await puppeteer.launch({
-    headless:        true,
-    args:            ['--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: null,
-  });
-  const lighthousePort = new URL(browser.wsEndpoint()).port;
-  const genDateTime    = new Date().toISOString();
-  const writeStream    = fs.createWriteStream(`${genDateTime.slice(0, 16)}.csv`);
+    const executablePath = process.pkg ? puppeteer.executablePath().replace(__dirname, '.') : puppeteer.executablePath();
+    // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
+    const browser = await puppeteer.launch({
+      // executablePath:  './node_modules/puppeteer/.local-chromium/mac-662092/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+      executablePath,
+      headless:        true,
+      args:            ['--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: null,
+    });
+    const lighthousePort = new URL(browser.wsEndpoint()).port;
+    const genDateTime    = new Date().toISOString();
+    const writeStream    = fs.createWriteStream(`${genDateTime.slice(0, 16)}.csv`);
 
-  const header = Object.values(auditsNameTitleMap);
-  header.unshift(...Object.values(categoriesNameTitleMap));
-  header.unshift(...inputColumnsHeader);
-  
-  const headerRow = csvStringify([header], { delimiter: ',' });
-  writeStream.write(headerRow);
+    const header = Object.values(auditsNameTitleMap);
+    header.unshift(...Object.values(categoriesNameTitleMap));
+    header.unshift(...inputColumnsHeader);
+    
+    const headerRow = csvStringify([header], { delimiter: ',' });
+    writeStream.write(headerRow);
 
-  for (const target of auditTargets) {
+    for (const target of auditTargets) {
 
-    console.log(`Device = ${target.Device} || URL = ${target.URL}`);
-    console.log(`Total: ${totalTasks} || Current: ${countingDown--}`);
+      console.log(`Device = ${target.Device} || URL = ${target.URL}`);
+      console.log(`Total: ${totalTasks} || Current: ${countingDown--}`);
 
-    const resultRow = [target[`${inputColumnsHeader[0]}`], target[`${inputColumnsHeader[1]}`]];
-    try {
-      config.settings.emulatedFormFactor = target.Device;
-      const { lhr } = await lighthouse(target.URL, { port: lighthousePort }, config);
-      const csv     = generateReportCSV(lhr);
-      const result  = csvParse(csv, {
-        columns:          true,
-        skip_empty_lines: true,
-        trim:             true,
-      });
+      const resultRow = [target[`${inputColumnsHeader[0]}`], target[`${inputColumnsHeader[1]}`]];
+      try {
+        config.settings.emulatedFormFactor = target.Device;
+        const { lhr } = await lighthouse(target.URL, { port: lighthousePort }, config);
+        const csv     = generateReportCSV(lhr);
+        const result  = csvParse(csv, {
+          columns:          true,
+          skip_empty_lines: true,
+          trim:             true,
+        });
 
-      result.forEach(item => {
-        let position        = orderMap[item.name];
-        resultRow[position] = item.displayValue;
-      });
+        result.forEach(item => {
+          let position        = orderMap[item.name];
+          resultRow[position] = item.displayValue;
+        });
 
-      Object.keys(lhr.categories).forEach(ck => {
-        let position        = orderMap[ck];
-        resultRow[position] = lhr.categories[ck].score;
-      });
+        Object.keys(lhr.categories).forEach(ck => {
+          let position        = orderMap[ck];
+          resultRow[position] = lhr.categories[ck].score;
+        });
 
-    } catch (e) {
-      console.log(e);
-    } finally {
-      const csvResult = csvStringify([resultRow], { delimiter: ',' });
-      writeStream.write(csvResult);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        const csvResult = csvStringify([resultRow], { delimiter: ',' });
+        writeStream.write(csvResult);
+      }
     }
-  }
 
-  return { browser, writeStream };
-})().then(async (resources) => {
-  const elapsed    = Date.now() - startTime;
-  const difference = new Date(elapsed);
-  console.log(`Total time to accomplish all tasks: ${difference.getHours()} hours ${difference.getMinutes()} minutes`);
-  resources.browser.close();
-  resources.writeStream.end();
-});
+    return { browser, writeStream };
+  })().then(async (resources) => {
+    const elapsed    = Date.now() - startTime;
+    const difference = new Date(elapsed);
+    console.log(`Total time to accomplish all tasks: ${difference.getHours()} hours ${difference.getMinutes()} minutes`);
+    resources.browser.close();
+    resources.writeStream.end();
+  });
+}
 
 const normalizeResult = (value) => {
   const matchNumber = /\d+/.exec(value);
